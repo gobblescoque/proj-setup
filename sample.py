12605950e3c1df05f1543366d529c8fd31868d9c
Kyle <skotekalx@gmail.com>
2025-05-03 09:16:53 -0600

    Add folder and file creation

--- sample.py
+++ sample.py
@@ -6,6 +6,12 @@ folders = ['docs', 'src', 'tests']
 proj_name = str(input("Enter the project name: "))
 
 for folder in folders:
-	os.system('mkdir $PWD/' +  folder)
+	os.system(f'mkdir $PWD/{folder}')
+os.system(f'mkdir $PWD/src/{proj_name}')
+os.system(f'touch $PWD/src/{proj_name}/__init__.py')
+os.system(f'touch $PWD/src/{proj_name}/sample.py')
 
-os.system('mkdir $PWD/src/' + proj_name)
+# sample_text = "Hello, World!"
+
+# Something is broken here, fix later
+os.system(f"echo 'print("Hello, World!")' >> $PWD/src/{proj_name}/sample.py")
