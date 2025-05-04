5e228bd33bb4498bbe481b49e15f4c7df6e7a1b4
Kyle <skotekalx@gmail.com>
2025-05-04 16:05:07 -0600

    Add CLI project creation flag/feature

--- src/proj_setup/layout.py
+++ src/proj_setup/layout.py
@@ -1,33 +1,76 @@
 import typer
 import os
+import subprocess
 from typing_extensions import Annotated
 
 def srclayout(
-	proj_name: Annotated[str, typer.Argument(help="Enter a name for the project")] = ""
+	proj_name: Annotated[str, typer.Argument(help="Enter a name for the \
+											 project")] = "",
+	cli: Annotated[bool, typer.Option(help="Sets up project for a cli app")] = False
+
 ):
 	"""
 	Create a basic file structure for a basic Python project using the src
 	layout. Creates a 'hello world' template.
+	TODO: Refactor code to use
 	"""
 
 	folders = ['docs', 'src', 'tests']
+	main_file = 'sample.py'
 
 	# Sets up the basic folder structure
 	for folder in folders:
 		os.system(f'mkdir $PWD/{folder}')
 
+	# Consider refactoring into a different function for dir/file creation
 	os.system(f'mkdir $PWD/src/{proj_name}')
 	os.system(f'touch $PWD/src/{proj_name}/__init__.py')
-	os.system(f'touch $PWD/src/{proj_name}/sample.py')
+	os.system(f'touch $PWD/src/{proj_name}/__main__.py')
+	os.system(f'touch $PWD/src/{proj_name}/{main_file}')
 
 	# Creates the sample source file
-	file = open(f"./src/{proj_name}/sample.py", "r+")
-	file.write(f'print("Hello, World!")')
+	mod_contents = ['import typer\n',
+					'from typing_extensions import Annotated\n',
+					'\n',
+	                'def hello(\n'
+	                '          to_print: Annotated[str, typer.Argument'\
+	                '(help="String to print to console")] = ""\n',
+	                '):\n',
+					'    print(to_print)']
+
+	file = open(f"./src/{proj_name}/{main_file}", "r+")
+	
+	for line in mod_contents:
+		file.write(line)
+
 	file.close()
 
+	# Creation of the boilerplate for a CLI project
+	if cli:
+		subprocess.run(["touch", "cli.py"], cwd=f'./src/{proj_name}/')
+
+		cli_contents = ['import typer\n',
+						'\n',
+						f'from .{main_file} import hello\n',
+						'\n',
+						'app = typer.Typer()\n',
+						'app.command()(hello)\n',
+						'\n',
+						'if __name__ == "__main__":\n',
+						'    app()'
+						]
+
+		file = open(f"./src/{proj_name}/cli.py", "r+")
+
+		for line in cli_contents:
+			file.write(line)
+		file.close()
+
 	# Creates the pyproject.toml
+	os.system(f'touch $PWD/pyproject.toml')
 
 	contents = ['[project]\n',
+				'name = "hello"\n',
 				'version = "0.0"\n',
 				'dependencies = []\n',
 				'\n',
@@ -39,12 +82,11 @@ def srclayout(
 				'[build-system]\n',
 				'\n',
 				'requires = ["hatchling >= 1.27"]\n',
-				'build-backend = "hatchling.build\n'
+				'build-backend = "hatchling.build"\n'
 	]
-
-	os.system(f'touch $PWD/pyproject.toml')
 
 	file = open(f"pyproject.toml", "r+")
 	for line in contents:
 		file.write(line)
 	file.close()
+
